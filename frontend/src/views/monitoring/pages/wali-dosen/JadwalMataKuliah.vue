630ef8a53cc84ec7f9d200180e17378cd69ae239
Ali Nurdin <aliiinrdn@gmail.com>
2021-12-01 21:09:54 +0700

    :lipstick: FrontEnd: Update hal. JadwalMataKuliah

--- frontend/src/views/monitoring/pages/wali-dosen/JadwalMataKuliah.vue
+++ frontend/src/views/monitoring/pages/wali-dosen/JadwalMataKuliah.vue
@@ -9,16 +9,18 @@
       </v-col>
       <v-col cols="12">
         <v-card elevation="0">
-          <v-card-title>Jadwal Mata Kuliah</v-card-title>
-          <v-card-subtitle>Kelas D4 - 3A, Semester 5 Tahun ajaran 2021/2022</v-card-subtitle>
-          <v-tabs fixed-tabs hide-slider v-model="tab" color="#525364">
-            <v-tab
-              v-for="day in days"
+          <p class="text-h4 font-weight-bold">Jadwal Mata Kuliah</p>
+          <p><span class="text-subtitle-1 font-weight-bold">Kelas D4 - 3A Semester 5</span><span class="text-subtitle-1"> Tahun ajaran 2021/2022</span><p>
+          <v-card-text class="text-subtitle-1">Pilih hari:</v-card-text>
+          <div class="v-item-group theme--light v-slide-group v-tabs-bar v-tabs-bar--is-mobile justify-space-around">
+            <button @click="buttonTabEvent"
+              v-for="(day, index) in days"
               :key="day"
-            >
-              <div>{{ day }}</div>
-            </v-tab>
-          </v-tabs>
+              :id="index"
+              class="v-btn v-btn--has-bg theme--light v-size--x-large rounded-lg font-weight-bold tab">
+            {{ day }}
+            </button>
+          </div>
         </v-card>
       </v-col>
       <v-col cols="12">
@@ -31,7 +33,7 @@
             <v-card class="pa-5 d-flex flex-no-wrap justify-space-between" style="gap:1rem">
                 <v-sheet rounded="lg" class="green lighten-4 pa-3">
                     <div
-                    class="rounded-lg pt-3 pb-6 text-h4 white"
+                    class="rounded-lg pt-3 pb-6 text-h4 white font-weight-bold"
                     :class="{'px-3' : isTablet, 'px-12' : !isTablet}"
                     >
                       1 - 2
@@ -41,7 +43,7 @@
                   <v-sheet>
                     <v-tooltip bottom>
                       <template v-slot:activator="{ on, attrs }">
-                        <div v-bind="attrs" v-on="on" class="text-h4 text-truncate" style="cursor:default">Manajemen Proyek Rekayasa Perangkat Lunak</div>
+                        <div v-bind="attrs" v-on="on" class="text-h4 text-truncate font-weight-bold" style="cursor:default">{{ item }} Manajemen Proyek Rekayasa Perangkat Lunak</div>
                       </template>
                       <span>Manajemen Proyek Rekayasa Perangkat Lunak</span>
                     </v-tooltip>
@@ -66,8 +68,8 @@
                   </v-sheet>
                 </v-sheet>
                 <v-sheet class="d-flex justify-space-between flex-column align-end">
-                  <v-sheet class="rounded-lg grey lighten-2 pa-3 text-h6">
-                    <div>07.00 - 08.40 : WIB</div>
+                  <v-sheet class="rounded-lg grey lighten-2 pa-3">
+                    <span class="text-h5 font-weight-bold">07.00 </span><span class="text-h5">- 08.40 : WIB</span>
                   </v-sheet>
                     <div class="rounded-lg green lighten-4 pa-3 text-h6">Teori</div>
                 </v-sheet>
@@ -101,7 +103,7 @@ export default {
           href: ""
         }
       ],
-      tab: null,
+      tab: 0,
       items: [0, 1, 2, 3, 4],
       days: ["SENIN", "SELASA", "RABU", "KAMIS", "JUMAT"]
     }
@@ -120,6 +122,17 @@ export default {
     identity: function () {
       return this.$store.getters.identity
     }
+  },
+  methods: {
+    buttonTabEvent (event) {
+      console.log(event.target.id)
+      this.tab = parseInt(event.target.id)
+    }
   }
 }
 </script>
+<style scoped>
+.tab {filter: drop-shadow(0px 7px 0px rgba(0, 0, 0, 0.25));background-color:white;}
+.tab:hover{background-color:#525364;}
+.tab:focus{background-color:#F7F7F7;filter: drop-shadow(0px 0px 0px rgba(0, 0, 0, 0))}
+</style>
